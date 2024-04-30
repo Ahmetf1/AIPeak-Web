bb0cb4b4c23601830f32c9e9022d64c918658860
afa <ahmetfakinci@gmail.com>
2024-04-30 16:44:56 +0300

    Remove unnecesarry widgets. Adjust text

--- lib/utilities/app_constants.dart
+++ lib/utilities/app_constants.dart
@@ -1,13 +1,13 @@
 // Almost all texts used through out the app.
 abstract class AppConstants {
   static const appTitle = 'AI-PEAK';
-  static const disclaimer = 'I do not claim ownership of the projects below as some of them were developed for companies I worked for and some for clients/clients of my clients while the backend side is provided.';
+  static const disclaimer = '';
   static const showcaseTitle = 'SHOWCASE';
-  static const showcaseDescription = 'MOBILE APPLICATIONS EITHER DEVELOPED COMPLETELY BY ME OR BY A TEAM WHERE I PARTICIPATED SIGNIFICANTLY';
+  static const showcaseDescription = 'TRY OUT OUR APPLICATIONS ENHANCED WITH ARTIFICIAL INTELLIGENCE';
   static const landingTitle = 'AI-PEAK';
   static const landingMotto = 'Strengthening Human Resources with Artificial Intelligence';
 
-  static const webSiteURL = 'kamranbekirov.com';
+  static const webSiteURL = 'https://ai-peak-web.vercel.app';
   static const gitHubProfileURL = 'https://github.com/kamranbekirovyz';
   static const linkedInProfileURL = 'https://linkedin.com/in/kamranbekirovyz';
   static const twitterURL = 'https://twitter.com/kamranbekirovyz';
